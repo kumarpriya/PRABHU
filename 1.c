e423620fba607c4f0867cbb269b44747976ed9f1
kumarpriya <30342256+kumarpriya@users.noreply.github.com>
2017-07-22 16:51:47 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,14 +1,1 @@
-#include<stdio.h>
- 
-int main() {
-   int a, b, sum;
- 
-   printf("\nEnter two no: ");
-   scanf("%d %d", &a, &b);
- 
-   sum = a + b;
- 
-   printf("Sum : %d", sum);
- 
-   return(0);
-}
+
